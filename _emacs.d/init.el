caedf7354b675b52a126f18a45bd4c1b8d626a6f
João Marques <mrshankly@users.noreply.github.com>
2019-01-12 15:24:38 +0000

    Set emacs font

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -20,18 +20,19 @@
 
 ;; Set site-lisp local path.
 (setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
+  (expand-file-name "site-lisp" user-emacs-directory))
 
 (add-to-list 'load-path site-lisp-dir)
 
 ;; Set themes local path.
 (setq themes-dir
-      (expand-file-name "themes" user-emacs-directory))
+  (expand-file-name "themes" user-emacs-directory))
 
 (add-to-list 'custom-theme-load-path themes-dir)
 
-;; Customize theme and font.
+;; Appearance
 (load-theme 'gruvbox-dark-hard t)
+(set-face-attribute 'default nil :font "Iosevka Term SS05-11")
 
 ;; Standard ML
 (autoload 'sml-mode "sml-mode" nil t)
@@ -41,17 +42,3 @@
 (setq rust-format-on-save t)
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
