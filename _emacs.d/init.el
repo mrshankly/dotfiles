5407748771fc56299f53d75eb9bd9aadac75786b
João Marques <mrshankly@users.noreply.github.com>
2019-01-12 14:17:35 +0000

    Add gruvbox theme and dependencies

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -24,6 +24,15 @@
 
 (add-to-list 'load-path site-lisp-dir)
 
+;; Set themes local path.
+(setq themes-dir
+      (expand-file-name "themes" user-emacs-directory))
+
+(add-to-list 'custom-theme-load-path themes-dir)
+
+;; Customize theme and font.
+(load-theme 'gruvbox t)
+
 ;; Standard ML
 (autoload 'sml-mode "sml-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.sml\\'" . sml-mode))
@@ -32,3 +41,17 @@
 (setq rust-format-on-save t)
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
