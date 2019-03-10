f518ef449a99d35e74c49dda1cf4181b3ed7970a
João Marques <mrshankly@users.noreply.github.com>
2019-03-10 14:36:42 +0000

    Enable company-mode only when programming

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -169,7 +169,7 @@
 ;; Text completion with `company'.
 (use-package company
   :hook
-  (after-init . global-company-mode)
+  (prog-mode . company-mode)
   :custom
   (company-idle-delay 0.2)
   (company-minimum-prefix-length 2)
